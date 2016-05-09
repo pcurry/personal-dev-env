450cdd9b99804f9fa9b67ea65fa3f814f19649c2
Paul Curry <paul.curry@karhoo.com>
2016-05-09 11:19:04 -0400

    skelleton for most Python command line scripts

---
+++ python_script_template.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import argparse
+
+
+def setup_parser():
+    parser = arparse.ArgumentParser(description="Your Name Here")
+    # More arguments here
+    return parser
+
+
+PARSER = setup_parser()
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
