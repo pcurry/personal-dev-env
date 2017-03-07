47b97b684acc5f048be4b28c13aa35ac5f8399fd
Paul Curry <pcurry@gmail.com>
2017-03-07 10:13:03 -0500

    Unittest boilerplate starter for Python 2 unittests

---
+++ boilerplate/python/python2_unittest.py
@@ -0,0 +1,26 @@
+'''
+Unittest boilerplate for the XUnit-family tests
+that come out of using the builtin Python module
+unittest.
+'''
+
+import unittest
+
+
+class TestThingie(unittest.TestCase):
+    '''
+    Stub testcase class boilerplate
+    '''
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
