00a1a48bbb35a680aca2da016d98fd0965af63b9
Paul Curry <pcurry@wellpass-pcurry.local>
2018-05-18 15:04:18 -0400

    Well, now at least it imports.

--- cleverness.py
+++ cleverness.py
@@ -1,3 +1,5 @@
+from collections import namedtuple
+
 def make_formatter(stub, value_map, none_replacement="None"):
     def formatter(value):
         if value is None:
@@ -5,7 +7,7 @@ def make_formatter(stub, value_map, none_replacement="None"):
         else:
             return stub.format(value_map.get(value, "unknown state"))
 
-Formater = namedtuple("Formater", ["stub", "map_function", "default"])
+Formatter = namedtuple("Formatter", ["stub", "map_function", "default"])
 
 
 def identity(the_thing):
@@ -15,4 +17,7 @@ def identity(the_thing):
 formatter_map = {
     "status": Formatter(
         "Coupon Status: {}",
-        constants.COUPON_STATUS_NAMES),
+        lambda x: x,
+        "Unknown"
+    )
+}
