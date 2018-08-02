54f86d093e92f7209e237662d69c554ec7197454
Paul Curry <pcurry@gmail.com>
2018-08-02 16:38:55 -0400

    WIP on aliases for my shell

--- shell-extensions/aliases.sh
+++ shell-extensions/aliases.sh
@@ -3,5 +3,12 @@
 alias 2irn='grep -2irn '
 alias lah='ls -lah'
 alias mmas='sudo'
+
+# git-specific aliases
+alias gfu='git fetch upstream'
+alias glom='git pull origin master'
+alias gpro='git pull --rebase origin'
+
+# OS X Specific aliases
 alias refreshbrew='brew update && brew upgrade && brew cleanup'
 alias hotcoffee=refreshbrew
