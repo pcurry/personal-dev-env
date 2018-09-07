aebadf171a146d6d5792d890e4a646bf26d30875
Paul Curry <pcurry@gmail.com>
2018-09-07 13:10:58 -0400

    A thing I was trying to get to, writing my own fontification

---
+++ emacs/closure-fontification.el
@@ -0,0 +1,5 @@
+;;; This is a stab at implementing fontifying the text in an emacs buffer
+;;; based on the syntactic closure in which it exists.
+
+;; I'm starting by reading http://www.lunaryorn.com/2014/06/16/advanced-syntactic-fontification.html
+
